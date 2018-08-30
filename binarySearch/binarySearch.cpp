1807fca5628caba8f54e0a96ca889cb7a2f18f1c
Unknown <faridmarin25@gmail.com>
2018-08-29 23:46:31 -0600

    BinarySearch ready
    The algorithm is ready but no testing yet

--- binarySearch/binarySearch.cpp
+++ binarySearch/binarySearch.cpp
@@ -2,4 +2,58 @@
 // Created by farida on 29/08/18.
 //
 
+#include <algorithm>
+#include <cmath>
 #include "binarySearch.h"
+
+binarySearch::binarySearch(const std::vector<int> &listToSearch) : listToSearch(listToSearch) {
+
+    sortList();
+
+}
+
+void binarySearch::sortList() {
+
+    std::sort(listToSearch.begin(),listToSearch.end());
+
+}
+
+void binarySearch::setNumberToFind(int numberToFind) {
+    binarySearch::numberToFind = numberToFind;
+}
+
+bool binarySearch::doesItExist() {
+
+    double size = listToSearch.size();
+    return doesItExistAux(0, size, (size-0)/2 );
+
+}
+
+
+bool binarySearch::doesItExistAux(double p1, double p2, double mid) {
+    mid = floor(mid);
+
+
+    if(listToSearch[mid] == numberToFind) {
+        return true;
+    }else if(p2 == p1+1){
+        if(p2 == numberToFind or p1 == numberToFind){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(numberToFind < mid){
+        p2 = mid;
+        mid = (p2 - p1)/2;
+        doesItExistAux(p1, p2, mid);
+    }else if(numberToFind > mid){
+        p1 = mid;
+        mid = mid+((p2 - p1)/2);
+        doesItExistAux(p1, p2, mid);
+    }
+
+
+    return false;
+}
+
+
--- binarySearch/binarySearch.h
+++ binarySearch/binarySearch.h
@@ -6,9 +6,27 @@
 #define SEARCHALGORITHMS_BINARYSEARCH_H
 
 
+#include <list>
+#include <vector>
+
 class binarySearch {
 
-    
+    std::vector<int> listToSearch;
+    int numberToFind = -10;
+
+public:
+    explicit binarySearch(const std::vector<int> &listToSearch);
+
+    void setNumberToFind(int numberToFind);
+
+    bool doesItExist();
+
+
+
+private:
+    void sortList();
+
+    bool doesItExistAux(double p1, double p2, double mid);
 
 };
 
